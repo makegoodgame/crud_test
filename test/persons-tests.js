50ca9e44ca134a22f8c53bd8a05a580c8709b399
jinyong Kim <jykim4250@naver.com>
2016-02-01 20:39:45 +0900

    쓸모없는 파일 정리

--- test/persons-tests.js
+++
@@ -1,9 +0,0 @@
-var Person = require('../models/persons');
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/crud_test');
-
-var p = new Person({ name:"Cristian", age:27 });
-p.save(function(err, doc){
-    console.log(err, doc);    
-});
